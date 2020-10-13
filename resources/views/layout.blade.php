e72d8629ca32fd095e415c58c774a80962d1a738
hytarys <68804163+hytarys@users.noreply.github.com>
2020-10-13 20:55:17 +0900

    Bootstrapの適用

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,10 +2,17 @@
 <html>
     <head>
         <meta charset='utf-8'>
+        <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
+        <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css' >
         <title>Lunchmap</title>
-        <style>body {padding: 80px;}</style>
+        <style>body {padding-top: 80px;}</style>
     </head>
     <body>
-        @yield('content')
+        <nav class='navbar navbar-expand-md navbar-dark bg-dark fixed-top'>
+            <a class='navbar-brand' href={{route('shop.list')}}>Lunchmap</a>
+        </nav>
+        <div class='container'>
+            @yield('content')
+        </div>
     </body>
 </html>
