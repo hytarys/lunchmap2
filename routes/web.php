c3dbf9dc934c7b345be8ada0f41e767a292a79fc
hytarys <68804163+hytarys@users.noreply.github.com>
2020-10-13 20:42:14 +0900

    shop一覧画面の作成

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/shops', 'App\Http\Controllers\ShopController@index')->name('shop.list');
+
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/shops');
 });
